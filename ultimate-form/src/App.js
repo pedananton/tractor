67360d7d45fcff81fd1aef5d86340f8de340612e
Anton Pedan <caravellejetta@gmail.com>
2020-10-19 20:54:10 +0300

    2054

--- ultimate-form/src/App.js
+++ ultimate-form/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+
+const Step1 = () => <>Step 1</>;
+const Step2 = () => <>Step 2</>;
+const Step3 = () => <>Step 3</>;
+const Result = () => <>Result</>;
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Step1} />
+          <Route path="/step2" component={Step2} />
+          <Route path="/step3" component={Step3} />
+          <Route path="/result" component={Result} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ ultimate-form/src/Step1.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { MainContainer } from "./components/MainContainer";
+import { Typography } from "@material-ui/core/Typography";
+import { Form } from "./components/Form";
+
+export const Step1 = () => {
+  return (
+    <MainContainer>
+      <Typography component="h2" variant="h5">
+        Step 1
+      </Typography>
+      <Form>
+        
+      </Form>
+    </MainContainer>
+  );
+};
